cf989b8cf3cc06b52b0673d023322505e6bddad4
Ehsan Vali <ehsan.vali@geeks.ltd.uk>
2023-08-09 16:27:05 +0100

    trace and devug with developer tools of brower

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        bundle: [ './app/app.js' ]
+      },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bpmn.js',
+      },
+  module: {
+    rules: [{
+        test: /\.bpmn$/,
+        use: 'raw-loader'
+      }]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'assets/**', to: 'vendor/bpmn-js', context: 'node_modules/bpmn-js/dist/' },
+      { from: '**/*.{html,css}', context: 'app/' }
+    ])
+  ],
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    // static: [
+    //   {
+    //     directory: path.join(__dirname, "dist"),
+    //     publicPath: "/dist",
+    //     watch: true,
+    //   },
+    // ],
+    // devMiddleware: {
+    //   writeToDisk: true,
+    // },
+    // liveReload: true,
+  //server: "http",
+  // host: "localhost",
+  port: "8081",
+  // hot: true,
+  // open: true,
+  // historyApiFallback: true,
+  // inline: true,
+  // publicPath: '/',
+    },
+  
+};
